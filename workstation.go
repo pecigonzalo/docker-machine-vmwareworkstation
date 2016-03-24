6e214b8cfa38238e308d830c1040382952267869
Gonzalo <pecigonzalo@outlook.com>
2016-03-24 09:23:18 +0100

    Create hardlink to normal mount folder for compose compatibility

--- workstation.go
+++ workstation.go
@@ -359,10 +359,12 @@ func (d *Driver) Create() error {
 		shareName = "Home"
 		shareDir = "/Home"
 		guestFolder = "/Users"
+		guestCompatLink = "/Home"
 	case "windows":
 		shareName = "Users"
 		shareDir = `C:\Users\`
 		guestFolder = "/Users"
+		guestCompatLink = "/c/Users"
 	}
 
 	if shareDir != "" {
@@ -371,7 +373,7 @@ func (d *Driver) Create() error {
 		} else if !os.IsNotExist(err) {
 			// add shared folder, create mountpoint and mount it.
 			vmrun("-gu", B2DUser, "-gp", B2DPass, "addSharedFolder", d.vmxPath(), shareName, shareDir)
-			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
+			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ ! -d" + guestCompatLink + " ]&& sudo ln -d " + guestFolder + " " + guestCompatLink + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
 			vmrun("-gu", B2DUser, "-gp", B2DPass, "runScriptInGuest", d.vmxPath(), "/bin/sh", command)
 		}
 	}
@@ -394,10 +396,12 @@ func (d *Driver) Start() error {
 		shareName = "Home"
 		shareDir = "/Home"
 		guestFolder = "/Users"
+		guestCompatLink = "/Home"
 	case "windows":
 		shareName = "Users"
 		shareDir = `C:\Users\`
 		guestFolder = "/Users"
+		guestCompatLink = "/c/Users"
 	}
 
 	if shareDir != "" {
@@ -405,7 +409,7 @@ func (d *Driver) Start() error {
 			return err
 		} else if !os.IsNotExist(err) {
 			// create mountpoint and mount shared folder
-			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
+			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ ! -d" + guestCompatLink + " ]&& sudo ln -d " + guestFolder + " " + guestCompatLink + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
 			vmrun("-gu", B2DUser, "-gp", B2DPass, "runScriptInGuest", d.vmxPath(), "/bin/sh", command)
 		}
 	}
