db08f454c06095d9abba3099cf111b2fbb500608
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2016-04-03 12:48:33 +0200

    Create hardlink to normal mount folder for compose compatibility
    * Create hardlink to normal mount folder for compose compatibility
    
    * Add variable declaration guestCompatLink
    
    * Update to golang 1.6

--- workstation.go
+++ workstation.go
@@ -353,16 +353,18 @@ func (d *Driver) Create() error {
 	// Enable Shared Folders
 	vmrun("-gu", B2DUser, "-gp", B2DPass, "enableSharedFolders", d.vmxPath())
 
-	var shareName, shareDir, guestFolder string // TODO configurable at some point
+	var shareName, shareDir, guestFolder, guestCompatLink string // TODO configurable at some point
 	switch runtime.GOOS {
-	case "linux":  // TODO Test linux working
+	case "linux": // TODO Test linux working
 		shareName = "Home"
 		shareDir = "/Home"
 		guestFolder = "/Users"
+		guestCompatLink = "/Home"
 	case "windows":
 		shareName = "Users"
 		shareDir = `C:\Users\`
 		guestFolder = "/Users"
+		guestCompatLink = "/c/Users"
 	}
 
 	if shareDir != "" {
@@ -371,7 +373,7 @@ func (d *Driver) Create() error {
 		} else if !os.IsNotExist(err) {
 			// add shared folder, create mountpoint and mount it.
 			vmrun("-gu", B2DUser, "-gp", B2DPass, "addSharedFolder", d.vmxPath(), shareName, shareDir)
-			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
+			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ ! -d" + guestCompatLink + " ]&& sudo ln -d " + guestFolder + " " + guestCompatLink + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
 			vmrun("-gu", B2DUser, "-gp", B2DPass, "runScriptInGuest", d.vmxPath(), "/bin/sh", command)
 		}
 	}
@@ -388,16 +390,18 @@ func (d *Driver) Start() error {
 	}
 
 	log.Debugf("Mounting Shared Folders...")
-	var shareName, shareDir, guestFolder string // TODO configurable at some point
+	var shareName, shareDir, guestFolder, guestCompatLink string // TODO configurable at some point
 	switch runtime.GOOS {
-	case "linux":  // TODO Test linux working
+	case "linux": // TODO Test linux working
 		shareName = "Home"
 		shareDir = "/Home"
 		guestFolder = "/Users"
+		guestCompatLink = "/Home"
 	case "windows":
 		shareName = "Users"
 		shareDir = `C:\Users\`
 		guestFolder = "/Users"
+		guestCompatLink = "/c/Users"
 	}
 
 	if shareDir != "" {
@@ -405,7 +409,7 @@ func (d *Driver) Start() error {
 			return err
 		} else if !os.IsNotExist(err) {
 			// create mountpoint and mount shared folder
-			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
+			command := "[ ! -d " + guestFolder + " ]&& sudo mkdir " + guestFolder + "; [ ! -d" + guestCompatLink + " ]&& sudo ln -d " + guestFolder + " " + guestCompatLink + "; [ -f /usr/local/bin/vmhgfs-fuse ]&& sudo /usr/local/bin/vmhgfs-fuse -o allow_other .host:/" + shareName + " " + guestFolder + " || sudo mount -t vmhgfs .host:/" + shareName + " " + guestFolder
 			vmrun("-gu", B2DUser, "-gp", B2DPass, "runScriptInGuest", d.vmxPath(), "/bin/sh", command)
 		}
 	}
